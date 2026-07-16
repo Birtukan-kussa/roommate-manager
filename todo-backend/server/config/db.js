b35aacbf3982af824f6e859e47d972b9d4091deb
BK <birtukankussa@gmail.com>
2026-07-17 01:27:48 +0300

    fix: correct env variable name for MongoDB URI

--- todo-backend/server/config/db.js
+++ todo-backend/server/config/db.js
@@ -3,7 +3,7 @@ import colors from "colors";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (err) {
     console.error(`Error: ${err.message}`.red.bold);
